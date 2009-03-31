11fb0a0c6b4054d8745c6323e05ab0e7e8758067
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-03-31 13:23:16 +0800

    Differentiate dropping in vs. out.
    Signed-off-by: Michael Fellinger <m.fellinger@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,16 @@ $( function() {
     $("#draggable").draggable();
 
     $("#droppable").droppable({
+        greedy: true,
         drop: function(event, ui) {
             $(this).addClass('ui-state-highlight').find('p').html('Plunk!');
+        }
+    });
+
+    $("#main").droppable({
+        drop: function(event, ui) {
+            $('#droppable').removeClass('ui-state-highlight');
+            $('#droppable > p').html('');
         }
     });
 } );
